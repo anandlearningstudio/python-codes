366d6f0c9bdcb7e7510387b483e7cb6815ba8dc8
anandlearningstudio <39327437+anandlearningstudio@users.noreply.github.com>
2019-03-25 10:43:59 +0530

    Updated to python3 syntax

--- pattern printing.py
+++ pattern printing.py
@@ -1,16 +1,15 @@
-
 n = int(input("Enter the starting number: "))
 m = int(input("Enter the number of rows: "))
 
 
 for i in range(0, m, 1):
     for j in range(0,i+1,1):
-        print n,
+        print(n, end=" ")
     n += 1
     print("\n")
 for i in range(m,0,-1):
     for j in range(0, i , 1):
-        print n-1,
+        print(n-1, end=" ")
     print("\n")
-    n -= 1
+n -= 1
 
